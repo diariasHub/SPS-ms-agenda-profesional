aa491b3dd59da6a175f7490e0e22d77bba4344e2
Manuel Cáceres <manuel.caceres@duocuc.cl>
2026-04-20 19:31:04 -0400

    Se sube ms-agenda a repositorio con configuracion inicial

---
+++ src/main/java/com/ms_agenda_profesional/agenda/config/FhierConfig.java
@@ -0,0 +1,22 @@
+package com.ms_agenda_profesional.agenda.config;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class FhierConfig {
+    
+    @Value("${fhir.server.url}")
+    private String ServerUrl;
+
+
+    @Bean
+    public IGenericClient fhirClient() {
+        FhirContext ctx = FhirContext.forR4();
+        return ctx.newRestfulGenericClient(ServerUrl);
+    }
+}
---
+++ src/main/java/com/ms_agenda_profesional/agenda/controller/AgendaController.java
@@ -0,0 +1,16 @@
+package com.ms_agenda_profesional.agenda.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+@RequestMapping("/centros")
+
+public class AgendaController {
+    
+    
+
+}
